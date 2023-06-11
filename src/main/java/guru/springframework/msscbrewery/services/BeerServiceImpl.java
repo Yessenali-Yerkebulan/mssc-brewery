e57322e6499df340b67cad54b68c56bcb712578c
Yerkebulan Yessenali <yerkebulanyessenali@gmail.com>
2023-06-11 18:42:28 +0600

    add HTTP PUT

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -19,4 +19,9 @@ public class BeerServiceImpl implements BeerService{
     public BeerDto saveNewBeer(BeerDto beerDto) {
         return beerDto.builder().id(UUID.randomUUID()).build();
     }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+
+    }
 }
